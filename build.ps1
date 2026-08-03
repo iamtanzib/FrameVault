c35528dfde08a39150f6967074b5313bd15986a8
tanzibgfx <tanzibhere@gmail.com>
2026-08-03 17:20:20 +0600

    Bump version to 1.0.8 for update test

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = "Stop"
-taskkill /F /IM FrameVault.exe 2>$null
+$ErrorActionPreference = "Continue"
+try { taskkill /F /IM FrameVault.exe 2>$null } catch {}
 cd "apps\standalone"
 npm run build -- --publish always
